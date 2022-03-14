af9e3b627c9dd8c0d9250bd9187d5e04115ec3cf
ismadiaaf <ismadiaafrilianti@gmail.com>
2022-03-14 15:12:20 +0700

    Inisial project pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,15 @@
+public class A_BlokDanKomentar {
+        public static void main(String[] args) {
+            System.out.println("Pemrograman Beorientasi Objek dengan Java");
+            //Contoh komentar 1 baris
+
+            /*
+            Contoh komentar lebih dari 1 baris
+             */
+
+            /**
+             * Java Doc--> baris ini untuk mendokumetasikan kode program
+             */
+        }
+
+}
