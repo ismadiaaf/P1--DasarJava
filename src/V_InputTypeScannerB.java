326877963edda7d2ef2d78a37cae641d40d9e2d6
ismadiaaf <ismadiaafrilianti@gmail.com>
2022-03-14 19:44:33 +0700

    Percobaan22- Mengkorversi nilai masukkan dari keyboard ke tipe tertentu menggunakan method bawaan kelas Scanner

---
+++ src/V_InputTypeScannerB.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class V_InputTypeScannerB {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nama : ");
+        String name = input.nextLine();
+        System.out.println("Masukkan umur : ");
+        int age = input.nextInt();
+        System.out.println("Masukkan tinggi badan : ");
+        double height = input.nextDouble();
+        System.out.println("Sudah menikah (true/false) : ");
+        boolean married = input.nextBoolean();
+        System.out.println("Nama : " + name);
+    }
+}
