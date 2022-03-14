80c7cb895ccafdbaa037be73593affaa758e8161
ismadiaaf <ismadiaafrilianti@gmail.com>
2022-03-14 16:50:37 +0700

    Percobaan13- Mencetak literal String

--- src/M_LiteralString.java
+++ src/M_LiteralString.java
@@ -1,4 +1,4 @@
-public class M_LiberakString {
+public class M_LiteralString {
     public static void main(String[] args) {
         String welcome = "Pemrograman Berorientasi Objek dengan Java";
         System.out.println(welcome);
