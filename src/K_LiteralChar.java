041961e3637f08ede0207eb8276fd5fcfd959607
ismadiaaf <ismadiaafrilianti@gmail.com>
2022-03-14 16:38:29 +0700

    Percobaan11- Mencetak literal Character

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,13 @@
+public class K_LiteralChar {
+    public static void main(String[] args) {
+        char tab = '\t';
+        char newline = '\n';
+        char singleQuote = '\'';
+        char doubleQuote = '\"';
+        System.out.println("Ini contoh" +tab+ "literal tab");
+        System.out.println("Ini contoh" +newline+ "literal newline");
+        System.out.println(""+ singleQuote+ "Ini contoh literal single quote" +singleQuote);
+        System.out.println(""+doubleQuote+ "Ini conroh literal double qoute" +doubleQuote);
+
+    }
+}
