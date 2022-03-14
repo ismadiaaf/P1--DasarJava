d7fe89a3878ada06103e4eadff66572414fb1982
ismadiaaf <ismadiaafrilianti@gmail.com>
2022-03-14 17:24:12 +0700

    Percobaan18- Membaca Masukan dari Keyboard dengan Dialog Box

---
+++ src/R_InputDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class R_InputDialog {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        System.out.println("My name is " +name);
+        System.exit((0));
+    }
+}
